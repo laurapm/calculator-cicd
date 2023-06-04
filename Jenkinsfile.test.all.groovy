2876ce5169fcc9583875e41599007863c734bf19
laurapm <32308528+laurapm@users.noreply.github.com>
2023-06-04 22:04:27 +0200

    Create Jenkinsfile.test.all.groovy
    Add API test

---
+++ Jenkinsfile.test.all.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        label 'docker'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
+        }
+        stage('Unit tests') {
+            steps {
+                sh 'make test-unit'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'results/*_result.xml'
+        }
+    }
+}
